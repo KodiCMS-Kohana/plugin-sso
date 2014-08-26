9700eefbc6d1bd60473ad3dfc37af48b10eccd12
Butschster <butschster@gmail.com>
2014-08-26 13:50:02 +0400

    init

---
+++ config/permissions.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+	'system' => array(
+		array(
+			'action' => 'social.settings',
+			'description' => 'Manage social accounts'
+		),
+	),
+);
